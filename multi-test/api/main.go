bb00a90fd0155739ee21781dc4ee526cd785ec6c
Ben Toogood <ben@micro.mu>
2020-02-25 09:47:39 +0000

    Update github repo syntax

--- multi-test/api/main.go
+++ multi-test/api/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("api #002")
+	fmt.Println("api #003")
 }
--- multi-test/main.go
+++ multi-test/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("main #002")
+	fmt.Println("main #003")
 }
