5662d27b2350f053921ac7d14b74d847ea63baa6
Ben Toogood <ben@micro.mu>
2020-02-25 09:42:09 +0000

    Trigger Test

--- multi-test/main.go
+++ multi-test/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("main #001")
+	fmt.Println("main #002")
 }
--- multi-test/web/main.go
+++ multi-test/web/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("web #001")
+	fmt.Println("web #002")
 }
