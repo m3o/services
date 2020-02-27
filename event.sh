240d6fa0c3ed7c87d3cfd6590fd5aa62f68045c3
Ben Toogood <ben@micro.mu>
2020-02-27 08:52:50 +0000

    Add build back to event.sh

--- event.sh
+++ event.sh
@@ -2,9 +2,12 @@
 
 # event is build.{started, failed, finished}
 EVENT=$1
+# build is the build number to be used at $REPO/actions/runs/$BUILD
+BUILD=$2
 # the url to send events to
 URL=https://micro.mu/platform/v1/github/events
 
 curl $URL -X POST -d @$HOME/changes.json \
 -H "Content-Type: application/json" \
+-H "X-Github-Build: $BUILD" \
 -H "Micro-Event: $EVENT"
