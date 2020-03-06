ed385ff8a530742208f002e78dfc3a93d2546030
Janos Dobronszki <dobronszki@gmail.com>
2020-03-06 15:47:07 +0100

    Creating service

--- subscribe/main.go
+++ subscribe/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	log "github.com/micro/go-micro/v2/logger"
-	"github.com/micro/go-micro/v2"
 	"subscribe/handler"
-	"subscribe/subscriber"
+
+	"github.com/micro/go-micro/v2"
+	log "github.com/micro/go-micro/v2/logger"
 
 	subscribe "subscribe/proto/subscribe"
 )
@@ -21,9 +21,6 @@ func main() {
 
 	// Register Handler
 	subscribe.RegisterSubscribeHandler(service.Server(), new(handler.Subscribe))
-
-	// Register Struct as Subscriber
-	micro.RegisterSubscriber("go.micro.srv.subscribe", service.Server(), new(subscriber.Subscribe))
 
 	// Run service
 	if err := service.Run(); err != nil {
