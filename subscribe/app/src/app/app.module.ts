1ac0fdfaca6067da33ff27364a0ada2ad82fb7a5
Janos Dobronszki <dobronszki@gmail.com>
2020-03-06 11:11:18 +0100

    Subscription works

--- subscribe/app/src/app/app.module.ts
+++ subscribe/app/src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { SubscribeFormComponent } from "./subscribe-form/subscribe-form.component";
-import { ClientModule, ClientService } from "@microhq/ng-client";
+import { ClientModule } from "@microhq/ng-client";
 import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { SubscriberListComponent } from './subscriber-list/subscriber-list.component';
 
 @NgModule({
-  declarations: [AppComponent, SubscribeFormComponent],
-  imports: [BrowserModule, AppRoutingModule, RouterModule, ClientModule],
+  declarations: [AppComponent, SubscribeFormComponent, SubscriberListComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    ClientModule,
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- subscribe/app/src/app/subscribe-form/subscribe-form.component.ts
+++ subscribe/app/src/app/subscribe-form/subscribe-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ClientService } from "@microhq/ng-client";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-subscribe-form",
@@ -8,13 +9,41 @@ import { ClientService } from "@microhq/ng-client";
   providers: []
 })
 export class SubscribeFormComponent implements OnInit {
+  email = "";
   subscribed: boolean = false;
+  domain = "";
+  error = "";
 
-  constructor(private mc: ClientService) {}
+  constructor(private mc: ClientService, private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.mc.call("go.micro.srv.greeter", "Say.Hello").then(response => {
-      console.log(response);
+    this.route.queryParams.subscribe(params => {
+      this.domain = params["domain"];
+      if (!this.domain || this.domain.length == 0) {
+        this.error =
+          "No domain parameter. Please embed this page with a domain query param.";
+        return;
+      }
     });
+  }
+
+  subscribe() {
+    if (!this.email) {
+      return
+    }
+    this.mc
+      .call("go.micro.store", "Store.Write", {
+        record: {
+          key: this.domain + ":" + this.email,
+          value: btoa(this.email)
+        }
+      })
+      .then(response => {
+        this.subscribed = true
+        console.log(response);
+      })
+      .catch(e => {
+        this.error = e;
+      });
   }
 }
