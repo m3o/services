66b503638bcb56d9a08673db1d1ad8f3f29db52e
Ben Toogood <ben@micro.mu>
2020-03-15 15:00:44 +0000

    Build Home Web

---
+++ home/web/app/build/precache-manifest.1e40dbfe9fbe7f262dbfe8153c3b32b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aff7fa4b1e39f8cf34c5fd53f5cd6aa7",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "faa8da212c34240cd345",
+    "url": "/home/static/css/main.cebf1034.chunk.css"
+  },
+  {
+    "revision": "ce11e5f297fdf6a3fac4",
+    "url": "/home/static/js/2.d1afe2ca.chunk.js"
+  },
+  {
+    "revision": "9a66f30a4c821dc412c03f85f8285901",
+    "url": "/home/static/js/2.d1afe2ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faa8da212c34240cd345",
+    "url": "/home/static/js/main.19096c30.chunk.js"
+  },
+  {
+    "revision": "e27c0f90f1286344ecc1",
+    "url": "/home/static/js/runtime-main.64cdd66f.js"
+  },
+  {
+    "revision": "905a19408bfe0d5a75a8d1c58a4113f7",
+    "url": "/home/static/media/background.905a1940.png"
+  }
+]);
