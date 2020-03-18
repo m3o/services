2781f114d43eba6fcd4aba28010321538065da58
Ben Toogood <ben@micro.mu>
2020-03-18 16:33:40 +0000

    Change team_id to team_slug

--- account/web/handler/handler.go
+++ account/web/handler/handler.go
@@ -39,25 +39,25 @@ func NewHandler(srv micro.Service) *Handler {
 	)
 
 	return &Handler{
-		google:       googleProv,
-		github:       githubProv,
-		githubTeamID: getConfigInt(srv, "github", "team_id"),
-		githubOrgID:  getConfigInt(srv, "github", "org_id"),
-		auth:         srv.Options().Auth,
-		users:        users.NewUsersService("go.micro.srv.users", srv.Client()),
-		login:        login.NewLoginService("go.micro.srv.login", srv.Client()),
+		google:         googleProv,
+		github:         githubProv,
+		githubOrgID:    getConfigInt(srv, "github", "org_id"),
+		githubTeamSlug: getConfigString(srv, "github", "team_slug"),
+		auth:           srv.Options().Auth,
+		users:          users.NewUsersService("go.micro.srv.users", srv.Client()),
+		login:          login.NewLoginService("go.micro.srv.login", srv.Client()),
 	}
 }
 
 // Handler is used to handle oauth logic
 type Handler struct {
-	githubTeamID int
-	githubOrgID  int
-	auth         auth.Auth
-	users        users.UsersService
-	login        login.LoginService
-	google       provider.Provider
-	github       provider.Provider
+	githubOrgID    int
+	githubTeamSlug string
+	auth           auth.Auth
+	users          users.UsersService
+	login          login.LoginService
+	google         provider.Provider
+	github         provider.Provider
 }
 
 func getConfigString(srv micro.Service, keys ...string) string {
