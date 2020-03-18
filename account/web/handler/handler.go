d4260c8c4540f72be765c90b5edb3cc6125ba88e
Ben Toogood <ben@micro.mu>
2020-03-18 12:41:26 +0000

    Change redirect err status

--- account/web/handler/handler.go
+++ account/web/handler/handler.go
@@ -67,7 +67,7 @@ func getConfig(srv micro.Service, keys ...string) string {
 
 func (h *Handler) handleError(w http.ResponseWriter, req *http.Request, format string, args ...interface{}) {
 	params := url.Values{"error": {fmt.Sprintf(format, args...)}}
-	http.Redirect(w, req, "/account?"+params.Encode(), http.StatusInternalServerError)
+	http.Redirect(w, req, "/account?"+params.Encode(), http.StatusFound)
 }
 
 func (h *Handler) loginUser(w http.ResponseWriter, req *http.Request, user *users.User, roleNames ...string) {
