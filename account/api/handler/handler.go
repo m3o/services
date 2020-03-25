a2aab59cdbebc77a0c7d67e0919e0caedf97f7a3
Ben Toogood <ben@micro.mu>
2020-03-25 09:04:45 +0000

    Fixes for elevated privelages on signup

--- account/api/handler/handler.go
+++ account/api/handler/handler.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
+	"fmt"
+
 	"github.com/micro/go-micro/v2"
 	"github.com/micro/go-micro/v2/auth"
+	log "github.com/micro/go-micro/v2/logger"
 
 	login "github.com/micro/services/login/service/proto/login"
 	payment "github.com/micro/services/payments/provider/proto"
@@ -13,6 +16,7 @@ import (
 type Handler struct {
 	name    string
 	auth    auth.Auth
+	account *auth.Account
 	users   users.UsersService
 	login   login.LoginService
 	payment payment.ProviderService
@@ -20,9 +24,17 @@ type Handler struct {
 
 // NewHandler returns an initialised handle
 func NewHandler(srv micro.Service) *Handler {
+	account, err := srv.Options().Auth.Generate(srv.Name(),
+		auth.WithRoles("service", fmt.Sprintf("service.%v", srv.Name())),
+	)
+	if err != nil {
+		log.Fatalf("Unable to generate service auth account: %v", err)
+	}
+
 	return &Handler{
 		name:    srv.Name(),
 		auth:    srv.Options().Auth,
+		account: account,
 		users:   users.NewUsersService("go.micro.service.users", srv.Client()),
 		login:   login.NewLoginService("go.micro.service.login", srv.Client()),
 		payment: payment.NewProviderService("go.micro.service.payment.stripe", srv.Client()),
