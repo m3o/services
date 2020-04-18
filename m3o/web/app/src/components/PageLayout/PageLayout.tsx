1c165d3464c9e9391515c283cd5c18197f9c493c
Ben Toogood <ben@micro.mu>
2020-04-18 11:07:49 +0100

    Nav Bar Icons

--- m3o/web/app/src/components/PageLayout/PageLayout.tsx
+++ m3o/web/app/src/components/PageLayout/PageLayout.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Logo from './assets/logo.png';
+import NavGettingStarted from './assets/nav-getting-started.png';
+import NavTeam from './assets/nav-team.png';
+import NavServices from './assets/nav-services.png';
+import NavConfiguration from './assets/nav-configuration.png';
+import NavBilling from './assets/nav-billing.png';
+import NavSettings from './assets/nav-settings.png';
 import './PageLayout.scss';
 
 interface Props {
@@ -12,30 +18,36 @@ export default class PageLayout extends React.Component<Props> {
     return(
       <div className='PageLayout'>
         <div className='sidebar'>
-          <img src={Logo} alt='M3O Logo' />
+          <img src={Logo} alt='M3O Logo' className='logo' />
 
           <nav>
             <NavLink exact to='/'>
+              <img src={NavGettingStarted} alt='Getting Started' />
               <p>Getting Started</p>
             </NavLink>
 
             <NavLink exact to='/team'>
+              <img src={NavTeam} alt='Team' />
               <p>Team</p>
             </NavLink>
 
             <NavLink exact to='/services'>
+              <img src={NavServices} alt='Services' />
               <p>Services</p>
             </NavLink>
 
             <NavLink exact to='/configuration'>
+              <img src={NavConfiguration} alt='Configuration' />
               <p>Configuration</p>
             </NavLink>
             
             <NavLink exact to='/billing'>
+              <img src={NavBilling} alt='Billing' />
               <p>Billing</p>
             </NavLink>
 
             <NavLink exact to='/settings'>
+              <img src={NavSettings} alt='Settings' />
               <p>Settings</p>
             </NavLink>
           </nav>
