29558174bec6408d0cb7d5f55e9047fce57e2592
Ben Toogood <ben@micro.mu>
2020-04-18 15:12:46 +0100

    Update main.go to serve react app

---
+++ m3o/web/app/build/precache-manifest.34eee684d9eab086029ecc153f486296.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26f156b746a9c6b733344a3da2a4f8d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08f3dd2fe1fea0fc71a6",
+    "url": "/static/css/main.8a77e005.chunk.css"
+  },
+  {
+    "revision": "18629433c20b38a58034",
+    "url": "/static/js/2.bc7cff7e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bc7cff7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08f3dd2fe1fea0fc71a6",
+    "url": "/static/js/main.11ea2686.chunk.js"
+  },
+  {
+    "revision": "12f8d51cfab65745940e",
+    "url": "/static/js/runtime-main.bd879a28.js"
+  }
+]);
