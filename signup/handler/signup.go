de3e9c667f993e9d72d94b932b87d5251983356d
Dominic Wong <dom@micro.mu>
2020-07-09 15:03:46 +0100

    double check its pulling in the local build

--- signup/handler/signup.go
+++ signup/handler/signup.go
@@ -119,7 +119,7 @@ func (e *Signup) SendVerificationEmail(ctx context.Context,
 		return merrors.Forbidden("go.micro.service.signup.notallowed", "user has not been invited to sign up")
 	}
 
-	k := randStringBytesMaskImprSrc(8)
+	k := randStringBytesMaskImprSrc(10)
 	tok := &tokenToEmail{
 		Token: k,
 		Email: req.Email,
