83ba69c5d7e81ea0a2a3c9aa33e92409330aae73
Dominic Wong <dom@m3o.com>
2020-07-31 12:36:17 +0100

    ugh

--- tests/integration/signup/signup_test.go
+++ tests/integration/signup/signup_test.go
@@ -78,7 +78,7 @@ func testM3oSignupFlow(t *test.T) {
 		if err != nil {
 			return outp, err
 		}
-		outp, err := exec.Command("micro", serv.EnvFlag(), "logs", "signup").CombinedOutput()
+		outp, err = exec.Command("micro", serv.EnvFlag(), "logs", "signup").CombinedOutput()
 		t.Log(string(outp))
 		if !strings.Contains(string(outp), "stripe") || !strings.Contains(string(outp), "signup") || !strings.Contains(string(outp), "invite") {
 			return outp, errors.New("Can't find signup or stripe or invite in list")
