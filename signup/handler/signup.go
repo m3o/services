a1219a44795c19a5900a4067d012a7c0eb2674b2
Janos Dobronszki <dobronszki@gmail.com>
2020-08-28 09:39:47 +0200

    Fix log

--- signup/handler/signup.go
+++ signup/handler/signup.go
@@ -359,9 +359,8 @@ func (e *Signup) Recover(ctx context.Context, req *signup.RecoverRequest, rsp *s
 			"namespace": v.Id,
 		})
 	}
-	if e.testMode {
-		logger.Infof("Sending email to %v with data %v", req.Email, namespaces)
-	}
+
+	logger.Infof("Sending email with data %v", namespaces)
 	return e.sendEmail(req.Email, e.recoverTemplateID, map[string]interface{}{
 		"namespaces": namespaces,
 	})
