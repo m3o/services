9280862f221a59cf7f26da1738ab76904a6f5ea5
Janos Dobronszki <dobronszki@gmail.com>
2020-09-02 15:50:21 +0200

    Moar logs

--- billing/handler/billing.go
+++ billing/handler/billing.go
@@ -141,6 +141,7 @@ func (b *Billing) loop() {
 					continue
 				}
 				if subsRsp == nil {
+					log.Warnf("Subscriptions listing response seems empty")
 					continue
 				}
 				for _, sub := range subsRsp.Subscriptions {
