b0de054f4a9109d46e913545f61fcdfcd6e815cd
Dominic Wong <dom@m3o.com>
2020-10-15 13:58:35 +0100

    make sure customer is marked active in free tier

--- customers/handler/events.go
+++ customers/handler/events.go
@@ -35,27 +35,63 @@ type SubscriptionModel struct {
 	ParentSubscriptionID string
 }
 
-func (c *Customers) consumeEvents() {
-	var evs <-chan mevents.Event
-	start := time.Now()
-	for {
-		var err error
-		evs, err = mevents.Subscribe("subscriptions",
-			mevents.WithAutoAck(false, 30*time.Second),
-			mevents.WithRetryLimit(10)) // 10 retries * 30 secs ackWait gives us 5 mins of tolerance for issues
-		if err == nil {
-			c.processSubscriptionEvents(evs)
-			start = time.Now()
-			continue // if for some reason evs closes we loop and try subscribing again
-		}
-		// TODO fix me
-		if time.Since(start) > 2*time.Minute {
-			logger.Fatalf("Failed to subscribe to subscriptions topic %s", err)
-		}
-		logger.Warnf("Unable to subscribe to evs %s. Will retry in 20 secs", err)
-		time.Sleep(20 * time.Second)
-	}
+type SignupEvent struct {
+	Type   string
+	Signup SignupModel
+}
 
+type SignupModel struct {
+	Email      string
+	Namespace  string
+	CustomerID string
+}
+
+func (c *Customers) consumeEvents() {
+	processTopic := func(topic string, handler func(ch <-chan mevents.Event)) {
+		var evs <-chan mevents.Event
+		start := time.Now()
+		for {
+			var err error
+			evs, err = mevents.Subscribe(topic,
+				mevents.WithAutoAck(false, 30*time.Second),
+				mevents.WithRetryLimit(10)) // 10 retries * 30 secs ackWait gives us 5 mins of tolerance for issues
+			if err == nil {
+				handler(evs)
+				start = time.Now()
+				continue // if for some reason evs closes we loop and try subscribing again
+			}
+			// TODO fix me
+			if time.Since(start) > 2*time.Minute {
+				logger.Fatalf("Failed to subscribe to topic %s: %s", topic, err)
+			}
+			logger.Warnf("Unable to subscribe to topic %s. Will retry in 20 secs. %s", topic, err)
+			time.Sleep(20 * time.Second)
+		}
+	}
+	go processTopic("subscriptions", c.processSubscriptionEvents)
+	go processTopic("signup", c.processSignupEvents)
+
+}
+
+func (c *Customers) processSignupEvents(ch <-chan mevents.Event) {
+	for ev := range ch {
+		se := &SignupEvent{}
+		if err := json.Unmarshal(ev.Payload, se); err != nil {
+			ev.Nack()
+			logger.Errorf("Error unmarshalling signup event: $s", err)
+			continue
+		}
+		switch se.Type {
+		case "signup.completed":
+			if _, err := updateCustomerStatusByID(se.Signup.CustomerID, statusActive); err != nil {
+				ev.Nack()
+				logger.Errorf("Error updating customers status for customer %s. %s", se.Signup.CustomerID, err)
+				continue
+			}
+			logger.Infof("Updated customer status to active from signup.completed event %+v", se)
+		}
+		ev.Ack()
+	}
 }
 
 func (c *Customers) processSubscriptionEvents(ch <-chan mevents.Event) {
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/alexellis/hmac v0.0.0-20180624211220-5c52ab81c0de
 	github.com/golang/protobuf v1.4.2
 	github.com/google/uuid v1.1.2
-	github.com/micro/go-micro/v3 v3.0.0-beta.3.0.20201013135405-1a962e46fd3a
+	github.com/micro/go-micro/v3 v3.0.0-beta.3
 	github.com/micro/micro/v3 v3.0.0-beta.6.0.20201015084013-5adee1bbfb5e
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/robfig/cron v1.2.0
