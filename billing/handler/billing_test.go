7efbcef37c0f5c4e2acf10d877aa3cf46f104cb6
Janos Dobronszki <dobronszki@gmail.com>
2020-10-28 13:40:09 +0100

    Changes

--- billing/handler/billing_test.go
+++ billing/handler/billing_test.go
@@ -20,51 +20,57 @@ func setupBillingTests() {
 
 type paymentMock struct {
 	sproto.ProviderService
+	ListSubscriptionsFunc func(ctx context.Context, in *sproto.ListSubscriptionsRequest, opts ...client.CallOption) (*sproto.ListSubscriptionsResponse, error)
 }
 
-func (u paymentMock) 	ListSubscriptions(ctx context.Context, in *sproto.ListSubscriptionsRequest, opts ...client.CallOption) (*sproto.ListSubscriptionsResponse, error) {
-	return nil, nil
+func (u paymentMock) ListSubscriptions(ctx context.Context, in *sproto.ListSubscriptionsRequest, opts ...client.CallOption) (*sproto.ListSubscriptionsResponse, error) {
+	return u.ListSubscriptionsFunc(ctx, in, opts...)
 }
 
 type namespaceMock struct {
 	nsproto.NamespacesService
+	ListFunc func(ctx context.Context, in *nsproto.ListRequest, opts ...client.CallOption) (*nsproto.ListResponse, error)
 }
 
 func (n namespaceMock) List(ctx context.Context, in *nsproto.ListRequest, opts ...client.CallOption) (*nsproto.ListResponse, error) {
-	return nil, nil
+	return n.ListFunc(ctx, in, opts...)
 }
 
 type usageMock struct {
 	uproto.UsageService
+	ReadFunc func (ctx context.Context, in *uproto.ReadRequest, opts ...client.CallOption) (*uproto.ReadResponse, error)
 }
 
 func (u usageMock) Read(ctx context.Context, in *uproto.ReadRequest, opts ...client.CallOption) (*uproto.ReadResponse, error) {
-	return nil, nil
+	return u.ReadFunc(ctx, in, opts...)
 }
 
 
 type subscriptionMock struct {
 	subproto.SubscriptionsService
+	UpdateFunc func(ctx context.Context, in *subproto.UpdateRequest, opts ...client.CallOption) (*subproto.UpdateResponse, error) 
 }
 
 func (u subscriptionMock) Update(ctx context.Context, in *subproto.UpdateRequest, opts ...client.CallOption) (*subproto.UpdateResponse, error) {
-	return nil, nil
+	return u.UpdateFunc(ctx, in, opts...)
 }
 
 type customersMock struct {
 	csproto.CustomersService
+	ReadFunc func(ctx context.Context, in *csproto.ReadRequest, opts ...client.CallOption) (*csproto.ReadResponse, error)
 }
 
-func (u customersMock) 	Read(ctx context.Context, in *csproto.ReadRequest, opts ...client.CallOption) (*csproto.ReadResponse, error) {
-	return nil, nil
+func (u customersMock) Read(ctx context.Context, in *csproto.ReadRequest, opts ...client.CallOption) (*csproto.ReadResponse, error) {
+	return u.ReadFunc(ctx, in, opts...)
 }
 
 type alertMock struct {
 	asproto.AlertService
+	ReportEventFunc func(ctx context.Context, in *asproto.ReportEventRequest, opts ...client.CallOption) (*asproto.ReportEventResponse, error)
 }
 
 func (u alertMock) 	ReportEvent(ctx context.Context, in *asproto.ReportEventRequest, opts ...client.CallOption) (*asproto.ReportEventResponse, error) {
-	return nil, nil
+	return u.ReportEventFunc(ctx, in, opts...)
 }
 
 func TestBillingCalc(t *testing.T) {
