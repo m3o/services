4c33b9e90fbe259b2ae65d2ea33901e24b7d376d
Dominic Wong <dom@m3o.com>
2020-11-10 10:29:41 +0000

    subscription cancellation tests + fix

--- subscriptions/handler/subscription_test.go
+++ subscriptions/handler/subscription_test.go
@@ -32,13 +32,17 @@ func mockedSubscription() *Subscriptions {
 	}
 }
 
-func TestSubCancellation(t *testing.T) {
+func TestSubCreateAndCancel(t *testing.T) {
 	g := NewWithT(t)
 	subSvc := mockedSubscription()
 	fstream := &fakes.FakeStream{}
 	mevents.DefaultStream = fstream
 	mstore.DefaultStore = memory.NewStore()
+	ppsvc := subSvc.paymentService.(*mprov.FakeProviderService)
+
 	adminCtx := mt.ContextWithAccount("micro", "foo")
+
+	// creation
 	cRsp := &pb.CreateResponse{}
 	err := subSvc.Create(adminCtx, &pb.CreateRequest{
 		CustomerID:      "1234",
@@ -47,17 +51,50 @@ func TestSubCancellation(t *testing.T) {
 		Email:           "foo@bar.com",
 	}, cRsp)
 	g.Expect(err).To(BeNil())
+
 	g.Expect(fstream.PublishCallCount()).To(Equal(1))
+	g.Expect(ppsvc.CreateCustomerCallCount()).To(Equal(1))
+	g.Expect(ppsvc.CreatePaymentMethodCallCount()).To(Equal(1))
+	g.Expect(ppsvc.SetDefaultPaymentMethodCallCount()).To(Equal(1))
+	g.Expect(ppsvc.CreateSubscriptionCallCount()).To(Equal(1))
 
 	recs, err := mstore.Read("", mstore.Prefix(prefixCustomer+"1234/"))
 	g.Expect(err).To(BeNil())
 	g.Expect(recs).To(HaveLen(1))
 
+	// add user
+	ppsvc.ListSubscriptionsReturnsOnCall(0, &mprovpb.ListSubscriptionsResponse{}, nil)
+	err = subSvc.AddUser(adminCtx, &pb.AddUserRequest{
+		OwnerID:   "1234",
+		NewUserID: "2345",
+	}, &pb.AddUserResponse{})
+	g.Expect(err).To(BeNil())
+	// adding the first user should create a new subscription
+	g.Expect(ppsvc.CreateSubscriptionCallCount()).To(Equal(2))
+	// check its created with the right arg
+	_, req, _ := ppsvc.CreateSubscriptionArgsForCall(1)
+	g.Expect(req.PriceId).To(Equal(subSvc.config.AdditionalUsersPriceID))
+	g.Expect(fstream.PublishCallCount()).To(Equal(2))
+
+	// add a second user
+	ppsvc.ListSubscriptionsReturnsOnCall(1, &mprovpb.ListSubscriptionsResponse{
+		Subscriptions: []*mprovpb.Subscription{&mprovpb.Subscription{Id: "sub_1234"}},
+	}, nil)
+	err = subSvc.AddUser(adminCtx, &pb.AddUserRequest{
+		OwnerID:   "1234",
+		NewUserID: "3456",
+	}, &pb.AddUserResponse{})
+	g.Expect(err).To(BeNil())
+	// adding the second user should update the subscription
+	g.Expect(ppsvc.CreateSubscriptionCallCount()).To(Equal(2))
+	g.Expect(ppsvc.UpdateSubscriptionCallCount()).To(Equal(1))
+	g.Expect(fstream.PublishCallCount()).To(Equal(3))
+
+	// cancellation
 	err = subSvc.Cancel(adminCtx, &pb.CancelRequest{
 		CustomerID: "1234",
 	}, &pb.CancelResponse{})
 	g.Expect(err).To(BeNil())
-	g.Expect(fstream.PublishCallCount()).To(Equal(2))
 
 	recs, err = mstore.Read("", mstore.Prefix(prefixCustomer+"1234/"))
 	g.Expect(err).To(BeNil())
@@ -65,6 +102,8 @@ func TestSubCancellation(t *testing.T) {
 	sub := &Subscription{}
 	g.Expect(json.Unmarshal(recs[0].Value, sub)).To(BeNil())
 	g.Expect(sub.Expires).To(BeNumerically("<", time.Now().Unix()+1))
-	ppsvc := subSvc.paymentService.(*mprov.FakeProviderService)
 	g.Expect(ppsvc.DeleteCustomerCallCount()).To(Equal(1))
+	// three more publishes; 1 for main sub cancel + 2 more for the child subs
+	g.Expect(fstream.PublishCallCount()).To(Equal(6))
+
 }
