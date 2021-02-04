1421257915353bf46a9e3900b2b42c55c20a38a9
Dominic Wong <dom@m3o.com>
2021-02-04 11:24:27 +0000

    switch back to normal auth

--- v1api/handler/v1api.go
+++ v1api/handler/v1api.go
@@ -341,10 +341,7 @@ func (e *V1) Endpoint(ctx context.Context, req *pb.Request, rsp *pb.Response) er
 	)
 
 	// set the auth
-	// use server auth to satisfy auth rules so that we can call services in the micro namespace
-	authOpts := auth.DefaultAuth.Options()
-	ctx = metadata.Set(ctx, "Authorization", fmt.Sprintf("Bearer %s", authOpts.Token.AccessToken))
-	ctx = metadata.Set(ctx, "Caller-Token", apiRec.Token)
+	ctx = metadata.Set(ctx, "Authorization", fmt.Sprintf("Bearer %s", apiRec.Token))
 
 	// create request/response
 	var response json.RawMessage
