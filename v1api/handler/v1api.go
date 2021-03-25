c263a52eb220cda3e8b8d035ae78fac2e73e80a0
Dominic Wong <dom@m3o.com>
2021-03-25 17:56:59 +0000

    moar debugging

--- v1api/handler/v1api.go
+++ v1api/handler/v1api.go
@@ -384,8 +384,10 @@ func parseContentType(ct string) string {
 // Endpoint is a catch all for endpoints
 func (e *V1) Endpoint(ctx context.Context, stream server.Stream) (retErr error) {
 	// check api key
+	var reqURL string
+	uid := uuid.New().String()
 	defer func() {
-		log.Infof("Closing stream")
+		log.Infof("Closing stream %s %s", reqURL, uid)
 		stream.Close()
 	}()
 
@@ -399,11 +401,12 @@ func (e *V1) Endpoint(ctx context.Context, stream server.Stream) (retErr error) 
 		return err
 	}
 
-	reqURL, ok := md.Get("url")
+	reqURL, ok = md.Get("url")
 	if !ok {
 		log.Errorf("Requested URL not found")
 		return errInternal
 	}
+	log.Infof("Received call %s %s", reqURL, uid)
 
 	if err := verifyCallAllowed(apiRec, reqURL); err != nil {
 		return err
