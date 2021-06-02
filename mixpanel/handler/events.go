3aeac2f4da991f86300ec5379d5109220b140515
Dominic Wong <dom@m3o.com>
2021-06-02 16:34:44 +0100

    refactor mixpanel

--- mixpanel/handler/events.go
+++ mixpanel/handler/events.go
@@ -13,38 +13,38 @@ import (
 )
 
 func (b *Mixpanel) consumeEvents() {
-
-	processTopic := func(topic string, handler func(ch <-chan mevents.Event)) {
-		var evs <-chan mevents.Event
-		start := time.Now()
-		for {
-			var err error
-			evs, err = mevents.Consume(topic,
-				mevents.WithAutoAck(false, 30*time.Second),
-				mevents.WithRetryLimit(10),
-				mevents.WithGroup(fmt.Sprintf("%s-%s", "mixpanel", topic))) // 10 retries * 30 secs ackWait gives us 5 mins of tolerance for issues
-			if err == nil {
-				handler(evs)
-				start = time.Now()
-				continue // if for some reason evs closes we loop and try subscribing again
-			}
-			// TODO fix me
-			if time.Since(start) > 2*time.Minute {
-				logger.Fatalf("Failed to subscribe to topic %s: %s", topic, err)
-			}
-			logger.Warnf("Unable to subscribe to topic %s. Will retry in 20 secs. %s", topic, err)
-			time.Sleep(20 * time.Second)
-		}
-	}
-	go processTopic("v1api", b.processV1apiEvents)
-	go processTopic("balance", b.processBalanceEvents)
-	go processTopic("customers", b.processCustomerEvents)
+	go b.processTopic("v1api", b.processV1APIEvent)
+	go b.processTopic("balance", b.processBalanceEvent)
+	go b.processTopic("customers", b.processCustomerEvent)
 }
 
-func (b *Mixpanel) processV1apiEvents(ch <-chan mevents.Event) {
-	logger.Infof("Starting to process v1api events")
+func (b *Mixpanel) processTopic(topic string, handler func(ev mevents.Event) error) {
+	var evs <-chan mevents.Event
+	start := time.Now()
 	for {
-		t := time.NewTimer(600 * time.Minute)
+		var err error
+		evs, err = mevents.Consume(topic,
+			mevents.WithAutoAck(false, 30*time.Second),
+			mevents.WithRetryLimit(10),
+			mevents.WithGroup(fmt.Sprintf("%s-%s", "mixpanel", topic))) // 10 retries * 30 secs ackWait gives us 5 mins of tolerance for issues
+		if err == nil {
+			logger.Infof("Starting to process %s events", topic)
+			b.processEvents(evs, handler)
+			start = time.Now()
+			continue // if for some reason evs closes we loop and try subscribing again
+		}
+		// TODO fix me
+		if time.Since(start) > 2*time.Minute {
+			logger.Fatalf("Failed to subscribe to topic %s: %s", topic, err)
+		}
+		logger.Warnf("Unable to subscribe to topic %s. Will retry in 20 secs. %s", topic, err)
+		time.Sleep(20 * time.Second)
+	}
+}
+
+func (b *Mixpanel) processEvents(ch <-chan mevents.Event, handler func(ev mevents.Event) error) {
+	for {
+		t := time.NewTimer(60 * time.Minute)
 		var ev mevents.Event
 		select {
 		case ev = <-ch:
@@ -56,35 +56,10 @@ func (b *Mixpanel) processV1apiEvents(ch <-chan mevents.Event) {
 			}
 		case <-t.C:
 			// safety net in case we stop receiving messages for some reason
-			logger.Infof("No messages received for last 2 minutes retrying connection")
+			logger.Infof("No messages received for last 60 minutes retrying connection")
 			return
 		}
-
-		ve := &v1api.Event{}
-
-		if err := json.Unmarshal(ev.Payload, ve); err != nil {
-			logger.Errorf("Error unmarshalling v1api event, discarding: $s", err)
-			ev.Ack()
-			continue
-		}
-
-		customerID := ""
-		switch ve.Type {
-		case "Request":
-			customerID = ve.Request.UserId
-		case "APIKeyCreate":
-			customerID = ve.ApiKeyCreate.UserId
-		case "APIKeyRevoke":
-			customerID = ve.ApiKeyRevoke.UserId
-		default:
-			logger.Infof("Event type for v1api not supported %s", ve.Type)
-			ev.Ack()
-			continue
-		}
-
-		mEv := b.client.newMixpanelEvent(ev.Topic, ve.Type, customerID, ev.ID, ev.Timestamp.Unix(), ve)
-		if err := b.client.Track(mEv); err != nil {
-			logger.Errorf("Error tracking event %s", err)
+		if err := handler(ev); err != nil {
 			ev.Nack()
 			continue
 		}
@@ -92,82 +67,66 @@ func (b *Mixpanel) processV1apiEvents(ch <-chan mevents.Event) {
 	}
 }
 
-func (b *Mixpanel) processBalanceEvents(ch <-chan mevents.Event) {
-	logger.Infof("Starting to process balance events")
-	for {
-		t := time.NewTimer(600 * time.Minute)
-		var ev mevents.Event
-		select {
-		case ev = <-ch:
-			t.Stop()
-			if len(ev.ID) == 0 {
-				// channel closed
-				logger.Infof("Channel closed, retrying stream connection")
-				return
-			}
-		case <-t.C:
-			// safety net in case we stop receiving messages for some reason
-			logger.Infof("No messages received for last 2 minutes retrying connection")
-			return
-		}
+func (b *Mixpanel) processV1APIEvent(ev mevents.Event) error {
+	ve := &v1api.Event{}
 
-		ve := &balance.Event{}
-
-		if err := json.Unmarshal(ev.Payload, ve); err != nil {
-			logger.Errorf("Error unmarshalling balance event, discarding: $s", err)
-			ev.Ack()
-			continue
-		}
-
-		customerID := ve.CustomerId
-
-		mEv := b.client.newMixpanelEvent(ev.Topic, ve.Type.String(), customerID, ev.ID, ev.Timestamp.Unix(), ve)
-		if err := b.client.Track(mEv); err != nil {
-			logger.Errorf("Error tracking event %s", err)
-			ev.Nack()
-			continue
-		}
-
-		ev.Ack()
+	if err := json.Unmarshal(ev.Payload, ve); err != nil {
+		logger.Errorf("Error unmarshalling v1api event, discarding: $s", err)
+		return nil
 	}
+
+	customerID := ""
+	switch ve.Type {
+	case "Request":
+		customerID = ve.Request.UserId
+	case "APIKeyCreate":
+		customerID = ve.ApiKeyCreate.UserId
+	case "APIKeyRevoke":
+		customerID = ve.ApiKeyRevoke.UserId
+	default:
+		logger.Infof("Event type for v1api not supported %s", ve.Type)
+		return nil
+	}
+
+	mEv := b.client.newMixpanelEvent(ev.Topic, ve.Type, customerID, ev.ID, ev.Timestamp.Unix(), ve)
+	if err := b.client.Track(mEv); err != nil {
+		logger.Errorf("Error tracking event %s", err)
+		return err
+	}
+	return nil
+
 }
 
-func (b *Mixpanel) processCustomerEvents(ch <-chan mevents.Event) {
-	logger.Infof("Starting to process customers events")
-	for {
-		t := time.NewTimer(600 * time.Minute)
-		var ev mevents.Event
-		select {
-		case ev = <-ch:
-			t.Stop()
-			if len(ev.ID) == 0 {
-				// channel closed
-				logger.Infof("Channel closed, retrying stream connection")
-				return
-			}
-		case <-t.C:
-			// safety net in case we stop receiving messages for some reason
-			logger.Infof("No messages received for last 2 minutes retrying connection")
-			return
-		}
-
-		ve := &customers.Event{}
-
-		if err := json.Unmarshal(ev.Payload, ve); err != nil {
-			logger.Errorf("Error unmarshalling balance event, discarding: $s", err)
-			ev.Ack()
-			continue
-		}
-
-		customerID := ve.Customer.Id
-
-		mEv := b.client.newMixpanelEvent(ev.Topic, ve.Type.String(), customerID, ev.ID, ev.Timestamp.Unix(), ve)
-		if err := b.client.Track(mEv); err != nil {
-			logger.Errorf("Error tracking event %s", err)
-			ev.Nack()
-			continue
-		}
-
-		ev.Ack()
+func (b *Mixpanel) processBalanceEvent(ev mevents.Event) error {
+	ve := &balance.Event{}
+	if err := json.Unmarshal(ev.Payload, ve); err != nil {
+		logger.Errorf("Error unmarshalling balance event, discarding: $s", err)
+		return nil
 	}
+	customerID := ve.CustomerId
+
+	mEv := b.client.newMixpanelEvent(ev.Topic, ve.Type.String(), customerID, ev.ID, ev.Timestamp.Unix(), ve)
+	if err := b.client.Track(mEv); err != nil {
+		logger.Errorf("Error tracking event %s", err)
+		return err
+	}
+
+	return nil
+}
+
+func (b *Mixpanel) processCustomerEvent(ev mevents.Event) error {
+	ve := &customers.Event{}
+	if err := json.Unmarshal(ev.Payload, ve); err != nil {
+		logger.Errorf("Error unmarshalling balance event, discarding: $s", err)
+		return nil
+	}
+	customerID := ve.Customer.Id
+
+	mEv := b.client.newMixpanelEvent(ev.Topic, ve.Type.String(), customerID, ev.ID, ev.Timestamp.Unix(), ve)
+	if err := b.client.Track(mEv); err != nil {
+		logger.Errorf("Error tracking event %s", err)
+		return err
+	}
+
+	return nil
 }
