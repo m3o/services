6187ad42d9112b90ee29e0c2a3775fc347dcd5ef
Dominic Wong <dom@m3o.com>
2021-06-09 17:50:31 +0100

    balance tracing

--- balance/main.go
+++ balance/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"github.com/m3o/services/balance/handler"
 	pb "github.com/m3o/services/balance/proto"
+	"github.com/micro/micro/v3/service/config"
+	"github.com/micro/micro/v3/util/opentelemetry/jaeger"
 
 	"github.com/micro/micro/v3/service"
 	"github.com/micro/micro/v3/service/logger"
+	"github.com/micro/micro/v3/util/opentelemetry"
 )
 
 func main() {
@@ -17,6 +20,19 @@ func main() {
 
 	// Register handler
 	pb.RegisterBalanceHandler(srv.Server(), handler.NewHandler(srv))
+
+	c, _ := config.Get("jaegeraddress")
+
+	openTracer, _, err := jaeger.New(
+		opentelemetry.WithServiceName("balance"),
+		opentelemetry.WithTraceReporterAddress(c.String("localhost:6831")),
+	)
+	if err != nil {
+		logger.Fatalf("Error configuring opentracing: %v", err)
+	}
+	logger.Infof("Configured jaeger to %s", c.String("localhost:6831"))
+
+	opentelemetry.DefaultOpenTracer = openTracer
 
 	// Run service
 	if err := srv.Run(); err != nil {
