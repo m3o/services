ecb15e52565915a67a07e3dd11400c2f4c17d717
Dominic Wong <dom@m3o.com>
2021-06-10 11:27:46 +0100

    fix up init

--- balance/main.go
+++ balance/main.go
@@ -5,6 +5,7 @@ import (
 	pb "github.com/m3o/services/balance/proto"
 	"github.com/micro/micro/v3/service/config"
 	"github.com/micro/micro/v3/util/opentelemetry/jaeger"
+	"github.com/opentracing/opentracing-go"
 
 	"github.com/micro/micro/v3/service"
 	"github.com/micro/micro/v3/service/logger"
@@ -23,16 +24,18 @@ func main() {
 
 	c, _ := config.Get("jaegeraddress")
 
-	openTracer, _, err := jaeger.New(
+	openTracer, tracecloser, err := jaeger.New(
 		opentelemetry.WithServiceName("balance"),
 		opentelemetry.WithTraceReporterAddress(c.String("localhost:6831")),
 	)
+	defer tracecloser.Close()
 	if err != nil {
 		logger.Fatalf("Error configuring opentracing: %v", err)
 	}
-	logger.Infof("Configured jaeger to %s", c.String("localhost:6831"))
-
+	// Set the global default opentracing tracer:
+	opentracing.SetGlobalTracer(openTracer)
 	opentelemetry.DefaultOpenTracer = openTracer
+	logger.Infof("Configured jaeger to %s", c.String("localhost:6831"))
 
 	// Run service
 	if err := srv.Run(); err != nil {
