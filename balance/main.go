d6ef61997dfef841606dfc94b2dffbaa62595586
Dominic Wong <dom@m3o.com>
2021-06-11 13:46:10 +0100

    tracing

--- balance/main.go
+++ balance/main.go
@@ -3,13 +3,10 @@ package main
 import (
 	"github.com/m3o/services/balance/handler"
 	pb "github.com/m3o/services/balance/proto"
-	"github.com/micro/micro/v3/service/config"
-	"github.com/micro/micro/v3/util/opentelemetry/jaeger"
-	"github.com/opentracing/opentracing-go"
+	"github.com/m3o/services/pkg/tracing"
 
 	"github.com/micro/micro/v3/service"
 	"github.com/micro/micro/v3/service/logger"
-	"github.com/micro/micro/v3/util/opentelemetry"
 )
 
 func main() {
@@ -22,20 +19,8 @@ func main() {
 	// Register handler
 	pb.RegisterBalanceHandler(srv.Server(), handler.NewHandler(srv))
 
-	c, _ := config.Get("jaegeraddress")
-
-	openTracer, tracecloser, err := jaeger.New(
-		opentelemetry.WithServiceName("balance"),
-		opentelemetry.WithTraceReporterAddress(c.String("localhost:6831")),
-	)
-	defer tracecloser.Close()
-	if err != nil {
-		logger.Fatalf("Error configuring opentracing: %v", err)
-	}
-	// Set the global default opentracing tracer:
-	opentracing.SetGlobalTracer(openTracer)
-	opentelemetry.DefaultOpenTracer = openTracer
-	logger.Infof("Configured jaeger to %s", c.String("localhost:6831"))
+	traceCloser := tracing.SetupOpentracing("balance")
+	defer traceCloser.Close()
 
 	// Run service
 	if err := srv.Run(); err != nil {
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/m3o/services
 go 1.14
 
 require (
-	github.com/cloudflare/cloudflare-go v0.10.9 // indirect
+	github.com/HdrHistogram/hdrhistogram-go v1.1.0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-redis/redis/v8 v8.5.0
@@ -11,12 +11,10 @@ require (
 	github.com/google/go-cmp v0.5.5 // indirect
 	github.com/google/uuid v1.1.2
 	github.com/gorilla/websocket v1.4.2
-	github.com/hashicorp/go-hclog v0.14.1 // indirect
 	github.com/hashicorp/golang-lru v0.5.4
-	github.com/klauspost/cpuid v1.3.1 // indirect
-	github.com/micro/micro/v3 v3.3.1-0.20210610212656-d96ddacd522b
+	github.com/micro/micro/v3 v3.3.1-0.20210611123551-3ed56b20d64f
 	github.com/onsi/gomega v1.10.5
-	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/opentracing/opentracing-go v1.2.0
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/sethvargo/go-diceware v0.2.0
@@ -27,5 +25,4 @@ require (
 	github.com/uber/jaeger-lib v2.4.1+incompatible // indirect
 	google.golang.org/grpc v1.27.0
 	google.golang.org/protobuf v1.25.0
-	gopkg.in/square/go-jose.v2 v2.4.1 // indirect
 )
